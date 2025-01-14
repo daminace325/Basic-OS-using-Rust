6f69efc37865723e67710a50f814e2661170ee52
acedamin325 <daminkhan325@gmail.com>
2025-01-14 20:55:47 +0530

    Implemented a bootloader and Qemu to display the text

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,19 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!"; //string Hello World in a variable
+
 #[no_mangle] //disable name mangling to ensure that the Rust compiler really outputs a function with the name _start
-pub extern "C" fn _start() -> ! {
+pub extern "C" fn _start() -> ! { //function written to print Hello World
+    let vga_buffer = 0xb8000 as *mut u8; //cast integer into a raw pointer
+
+    for (i, &byte) in HELLO.iter().enumerate() { //iterate over the bytes of static HELLO byte string
+        unsafe {  //using unsafe beacuse of raw pointer as they can point anywhere
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; //color 0xb(light cyan)
+        }
+    }
+
     loop {}
 }
 
