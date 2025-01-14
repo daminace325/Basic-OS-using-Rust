755406ab2e53b34f9167308fd298bcd1486e277d
acedamin325 <daminkhan325@gmail.com>
2025-01-14 19:28:54 +0530

    Disabled Unwinding and Overwriting the system point entry with own _start function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 #![no_std] //disable standard library
+#![no_main] //telling the rust compiler not to use the normal entry point chain, thus removing main function
 
 use core::panic::PanicInfo;
 
-fn main() {
+#[no_mangle] //disable name mangling to ensure that the Rust compiler really outputs a function with the name _start
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
-
 
 //function called on panic
 #[panic_handler] 
