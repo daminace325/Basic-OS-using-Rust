002fde5539521e0d541b0717c3cf0814f4834551
acedamin325 <daminkhan325@gmail.com>
2025-01-15 22:16:11 +0530

    Completed CPU exception handling and Double Faults handling

---
+++ src/interrupts.rs
@@ -0,0 +1,40 @@
+use x86_64::structures::idt::{
+    InterruptDescriptorTable,  //imported IDT(InterruptDescriptorTable) in a struct to specify a hanlder for each exception
+    InterruptStackFrame   //stack to save the state of CPU just before interrupt occurs
+};
+use crate::println;
+use lazy_static::lazy_static; //to make idt stactic since idt on its own is treated as a normal hence its time doesnt live long enough for interrupt handling
+use crate::gdt;
+
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = {
+        let mut idt = InterruptDescriptorTable::new(); //creates a new IDT
+        idt.breakpoint.set_handler_fn(breakpoint_handler); //add breakpoint handler into the IDT
+        unsafe{
+            idt.double_fault.set_handler_fn(double_fault_handler) //double fault handler to handler exceptions who do not have a handler in IDT 
+            .set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX); //to set the stack index for double fault handler in the IDT
+        }
+        idt
+    };
+}
+
+pub fn init_idt() { //func to initialize idt
+    IDT.load();
+}
+
+//just outputs a message and pretty-prints the interrupt stack frame.
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame){
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(stack_frame: InterruptStackFrame, _error_code: u64) -> !{
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+//breakpoint exception test
+#[test_case]
+fn test_breakpoint_exception() {
+    // invoke a breakpoint exception
+    x86_64::instructions::interrupts::int3();
+}
