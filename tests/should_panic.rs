01b14c209fcec924a9314cd45cf194c7951aa467
acedamin325 <daminkhan325@gmail.com>
2025-01-15 20:33:21 +0530

    Disabled the harness test for should_panic

--- tests/should_panic.rs
+++ tests/should_panic.rs
@@ -1,43 +1,28 @@
 //run cargo test --test should_panic
+
 #![no_std]
 #![no_main]
-#![feature(custom_test_frameworks)]
-#![test_runner(test_runner)]
-#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use rust_os::{QemuExitCode, exit_qemu, serial_println};
-use rust_os::serial_print;
+use rust_os::{exit_qemu, serial_print, serial_println, QemuExitCode};
+
+//since harness test is disabled, the test is treated like a normal executable.
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    should_fail();
+    serial_println!("[test did not panic]");
+    exit_qemu(QemuExitCode::Failed);
+    loop{}
+}
+
+fn should_fail() {
+    serial_print!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     serial_println!("[ok]");
     exit_qemu(QemuExitCode::Success);
     loop {}
-}
-
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-    test_main();
-
-    loop {}
-}
-
-
-//this test_runner exits with a failure exit code when a test returns without panicking
-pub fn test_runner(tests: &[&dyn Fn()]) { 
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test();
-        serial_println!("[test did not panic]");
-        exit_qemu(QemuExitCode::Failed);
-    }
-    exit_qemu(QemuExitCode::Success);
-}
-
-
-#[test_case]
-fn should_fail() {
-    serial_print!("should_panic::should_fail...\t");
-    assert_eq!(0, 1);
 }
