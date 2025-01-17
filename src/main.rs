87ce68ea0081017b74d6c5a5dcaca8129c41b9fc
acedamin325 <daminkhan325@gmail.com>
2025-01-17 21:30:20 +0530

    Completed Hardware Interrupt Handling

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+#![no_std] //disable Rust-standard library
+#![no_main] //disable the rust compiler not to use the normal entry point chain, thus removing main function
+#![feature(custom_test_frameworks)] //a test framework from rust on bare metal environment
+#![test_runner(rust_os::test_runner)]
+#![reexport_test_harness_main = "test_main"] //to change the name of the generated function to call test_main()
+
+use core::panic::PanicInfo;
+use rust_os::println;
+
+
+#[no_mangle] //disable name mangling to ensure that the Rust compiler really outputs a function with the name _start
+pub extern "C" fn _start() -> ! {  //start function
+    println!("Hello World{}", "!"); //directly use println! function using macros
+
+    rust_os::init();
+
+    #[cfg(test)] //ensure the call only happens during tests
+    test_main();
+
+    //if this is printed that means exceptions are being handled
+    println!("It did not crash!");
+
+    rust_os::hlt_loop();
+}
+
+//function called on panic
+#[cfg(not(test))] //use panic handler also on testing
+#[panic_handler] 
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
+    rust_os::hlt_loop();
+}
+
+
+//panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);
+}
