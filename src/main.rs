7d29a2edc98e4b4c85d6171fe025b93c502d3a09
acedamin325 <daminkhan325@gmail.com>
2025-01-18 00:45:40 +0530

    Implement address translation functionality and update kernel_main to demonstrate usage

--- src/main.rs
+++ src/main.rs
@@ -13,17 +13,28 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {  //start function
     use rust_os::memory::active_level_4_table;
     use x86_64::VirtAddr;
+    use rust_os::memory::translate_addr;
 
     println!("Hello World{}", "!");
     rust_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() { //print non-empty entries of pages
-            println!("L4 Entry {}: {:?}", i, entry);
-        }
+    let addresses = [
+        // the identity-mapped vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
+
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)] //ensure the call only happens during tests
