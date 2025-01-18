9db8f6ad3fb33b98aa53faa3686557c4753d7c7b
acedamin325 <daminkhan325@gmail.com>
2025-01-18 18:30:42 +0530

    Implemented the Stream trait for ScancodeStream

--- src/task/keyboard.rs
+++ src/task/keyboard.rs
@@ -1,12 +1,15 @@
 use conquer_once::spin::OnceCell;
 use crossbeam_queue::ArrayQueue;
 use crate::println;
+use core::{pin::Pin, task::{Poll, Context}};
+use futures_util::stream::Stream;
+use futures_util::task::AtomicWaker;
 
 pub struct ScancodeStream {
     _private: (),
 }
 
-
+static WAKER: AtomicWaker = AtomicWaker::new();
 static SCANCODE_QUEUE: OnceCell<ArrayQueue<u8>> = OnceCell::uninit();
 
 //called by the keyboard interrupt handler
@@ -14,6 +17,8 @@ pub(crate) fn add_scancode(scancode: u8) {
     if let Ok(queue) = SCANCODE_QUEUE.try_get() {
         if let Err(_) = queue.push(scancode) {
             println!("WARNING: scancode queue full; dropping keyboard input");
+        } else {
+            WAKER.wake(); //adding waker
         }
     } else {
         println!("WARNING: scancode queue uninitialized");
@@ -28,4 +33,26 @@ impl ScancodeStream {
     }
 }
 
+impl Stream for ScancodeStream {
+    type Item = u8;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Option<u8>> {
+        let queue = SCANCODE_QUEUE
+            .try_get()
+            .expect("scancode queue not initialized");
+
+        if let Some(scancode) = queue.pop() {
+            return Poll::Ready(Some(scancode));
+        }
+
+        WAKER.register(&cx.waker());
+        match queue.pop() {
+            Some(scancode) => {
+                WAKER.take();
+                Poll::Ready(Some(scancode))
+            }
+            None => Poll::Pending,
+        }
+    }
+}
 
