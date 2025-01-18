6a661ab2c8cbe5d4e100e25ef42e3b34138c44b4
acedamin325 <daminkhan325@gmail.com>
2025-01-18 16:39:01 +0530

    Reverting back changes due to unintended error

--- src/allocator.rs
+++ src/allocator.rs
@@ -14,7 +14,6 @@ use linked_list_allocator::LockedHeap;
 pub struct Dummy;
 pub const HEAP_START: usize = 0x_4444_4444_0000; //memory starting address
 pub const HEAP_SIZE: usize = 100 * 1024; //set heap size to 100 KiB
-pub mod bump;
 
 
 unsafe impl GlobalAlloc for Dummy {
@@ -60,31 +59,4 @@ pub fn init_heap(
     }
 
     Ok(())
-}
-
-
-pub struct BumpAllocator {
-    heap_start: usize,
-    heap_end: usize,
-    next: usize,
-    allocations: usize,
-}
-
-impl BumpAllocator {
-    /// Creates a new empty bump allocator.
-    pub const fn new() -> Self {
-        BumpAllocator {
-            heap_start: 0,
-            heap_end: 0,
-            next: 0,
-            allocations: 0,
-        }
-    }
-
-    /// Initializes the bump allocator with the given heap bounds.
-    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
-        self.heap_start = heap_start;
-        self.heap_end = heap_start + heap_size;
-        self.next = heap_start;
-    }
 }
